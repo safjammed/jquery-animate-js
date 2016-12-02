c9d7b429e733cc70c822f1f53fa968eb17458de3
Safayat Jamil <safayatjamil27@gmail.com>
2016-12-02 23:46:31 +0600

    Add files via upload

---
+++ Examples/test/Arrays_Of_Animation_Names.js
@@ -0,0 +1,33 @@
+/*
+
+I have Created an array of all animate.css and/or jquery.animate.js animation names
+Please Note that these are just arrays of animation names, not codes or anything. If
+you want to use these on your site please download animate.css or jquery.animate.js
+
+Coded By: safjammed
+
+Facebook: Facebook.com/saf.jammed
+
+Use it however you like :)
+*/
+
+
+//ALL ANIMATION NAME IN ONE ARRAY
+var Animations =["bounce","flash","pulse","rubberBand","shake","swing","tada","wobble","jello","bounceIn","bounceInDown","bounceInLeft","bounceInRight","bounceInUp","bounceOut","bounceOutDown","bounceOutLeft","bounceOutRight","bounceOutUp","fadeIn","fadeInDown","fadeInDownBig","fadeInLeft","fadeInLeftBig","fadeInRight","fadeInRightBig","fadeInUp","fadeInUpBig","fadeOut","fadeOutDown","fadeOutDownBig","fadeOutLeft","fadeOutLeftBig","fadeOutRight","fadeOutRightBig","fadeOutUp","fadeOutUpBig","flip","flipInX","flipInY","flipOutX","flipOutY","lightSpeedIn","lightSpeedOut","rotateIn","rotateInDownLeft","rotateInDownRight","rotateInUpLeft","rotateInUpRight","rotateOut","rotateOutDownLeft","rotateOutDownRight","rotateOutUpLeft","rotateOutUpRight","slideInUp","slideInDown","slideInLeft","slideInRight","slideOutUp","slideOutDown","slideOutLeft","slideOutRight","zoomIn","zoomInDown","zoomInLeft","zoomInRight","zoomInUp","zoomOut","zoomOutDown","zoomOutLeft","zoomOutRight","zoomOutUp","hinge","rollIn","rollOut"];
+
+//CATEGORIZED ANIMATION NAMES BASED ON ANIMATION TYPE
+var AttentionSeekers =["bounce","flash","pulse","rubberBand","shake","swing","tada","wobble","jello"];
+var BouncingEntrances  =["bounceIn","bounceInDown","bounceInLeft","bounceInRight","bounceInUp"];
+var BouncingExits =["bounceOut","bounceOutDown","bounceOutLeft","bounceOutRight","bounceOutUp"];
+var FadingEntrances  = ["fadeIn","fadeInDown","fadeInDownBig","fadeInLeft","fadeInLeftBig","fadeInRight","fadeInRightBig","fadeInUp","fadeInUpBig"];
+var FadingExits = ["fadeOut","fadeOutDown","fadeOutDownBig","fadeOutLeft","fadeOutLeftBig","fadeOutRight","fadeOutRightBig","fadeOutUp","fadeOutUpBig"];
+var Flippers =["flip","flipInX","flipInY","flipOutX","flipOutY"];
+var Lightspeed =["lightSpeedIn","lightSpeedOut"];
+var RotatingEntrances =["rotateIn","rotateInDownLeft","rotateInDownRight","rotateInUpLeft","rotateInUpRight"];
+var RotatingExits =["rotateOut","rotateOutDownLeft","rotateOutDownRight","rotateOutUpLeft","rotateOutUpRight"];
+var SlidingEntrances =["slideInUp","slideInDown","slideInLeft","slideInRight"];
+var SlidingExits =["slideOutUp","slideOutDown","slideOutLeft","slideOutRight"];
+var ZoomEntrances =["zoomIn","zoomInDown","zoomInLeft","zoomInRight","zoomInUp"];
+var ZoomExits =["zoomOut","zoomOutDown","zoomOutLeft","zoomOutRight","zoomOutUp"];
+var Specials =["hinge","rollIn","rollOut"];
+
